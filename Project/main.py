ad70fdcaa7122ef78942124b1a3ce5ad9b83ef65
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-04-14 02:37:28 +0400

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -8,18 +8,25 @@ data_path = 'data/MIGREN.csv'
 data = load_data(data_path)
 X, y = preprocess_data(data)
 X_train, X_test, y_train, y_test = split_data(X, y)
+print("Calculations are being made.")
 
 knn_best, knn_score = train_knn(X_train, y_train)
-print(f'Best KNN Score: {knn_score}')
+print("KNN score calculated.")
+
 
 svm_best, svm_score = train_svm(X_train, y_train)
-print(f'Best SVM Score: {svm_score}')
+print("SVM score calculated.")
 
 lr_best = train_lr(X_train, y_train)
 accuracy_knn = accuracy_score(y_test, knn_best.predict(X_test))
 accuracy_svm = accuracy_score(y_test, svm_best.predict(X_test))
 accuracy_lr = accuracy_score(y_test, lr_best.predict(X_test))
+print("Accuracy scores calculated.")
 
+print("-----------------------------------")
+print(f'Best KNN Score: {knn_score}')
+print(f'Best SVM Score: {svm_score}')
 print(f'KNN Accuracy: {accuracy_knn}')
 print(f'SVM Accuracy: {accuracy_svm}')
 print(f'LR Accuracy: {accuracy_lr}')
+print("-----------------------------------")
